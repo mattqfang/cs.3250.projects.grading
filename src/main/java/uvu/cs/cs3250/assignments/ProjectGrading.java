696bace7c66041c8867dada3d22e72fe7699e44b
Brandenfascher <jonortim@hotmail.com>
2019-11-25 23:43:43 -0700

    converted to using Student class object

--- src/main/java/uvu/cs/cs3250/assignments/ProjectGrading.java
+++ src/main/java/uvu/cs/cs3250/assignments/ProjectGrading.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,6 +13,7 @@ public class ProjectGrading {
 	
 	public static void main(String[] args) {
 		List<String[]> stringContents = new ArrayList<>();
+		List<Student> students;
 		
 		try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/data.txt"))) {
 			stringContents = stream
@@ -21,22 +23,82 @@ public class ProjectGrading {
 			System.out.println("Exception has occurred: " + e);
 		}
 		
+		students = stringContents.stream()
+			.map(s -> new Student(s[0], s[1], Integer.parseInt(s[2])))
+			.collect(Collectors.toList());
+		
 		//Print out each list of string array contents
-		stringContents.stream()
-			.map(k -> k[0] + " " + k[1] + " " + k[2])
+		students.stream()
+			.map(k -> k.getFirstName() + " " + k.getLastName() + " " + k.getGrade())
 			.forEach(System.out::println);
 		
-		System.out.println(findStudentGrade("mike", "conley", stringContents));
+		System.out.println(findStudentGrade("mike", "conley", students));
 	}
 	
 	
-	public static String findStudentGrade(String firstName, String lastName, List<String[]> students) {
+	public static int findStudentGrade(String firstName, String lastName, List<Student> students) {
 		return students.stream()
-			.filter(p -> p[0].equalsIgnoreCase(firstName) &&
-					p[1].equalsIgnoreCase(lastName))
+			.filter(s -> s.getFirstName().equalsIgnoreCase(firstName) &&
+					s.getLastName().equalsIgnoreCase(lastName))
 			.findFirst()
-			.map(p -> p[2])
-			.orElse("Grade not available");
+			.map(s -> s.getGrade())
+			.orElse(0);
 	}
-
+/*
+	public static String findStudentWithMaxGrade(List<String[]> students) {
+		return students.stream()
+			.sorted(Comparator.comparing(String[].class))
+			.findFirst()
+			.map(s -> s[0] + " " + s[1])
+			.orElseGet(() -> {
+				System.out.println("cannot find this dude");
+				return null;
+			});
+	}
+	
+	public static Double findAverageAgeByCity(String city, List<Person> allPersons) {
+		return allPersons.stream()
+			.filter(p -> p.getAddress() != null)
+			.filter(p -> city.equalsIgnoreCase(p.getAddress().getCity()))
+			.mapToInt(p -> p.getAge())
+			.average()
+			.getAsDouble();
+	}
+	*/
+	
+	static class Student {
+		private String firstName;
+		private String lastName;
+		private int grade;
+		
+		public Student(String firstName, String lastName, int grade) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.grade = grade;
+		}
+		
+		public String getFirstName( ) {
+			return firstName;
+		}
+		
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		
+		public String getLastName( ) {
+			return lastName;
+		}
+		
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		
+		public int getGrade( ) {
+			return grade;
+		}
+		
+		public void setGrade(int grade) {
+			this.grade = grade;
+		}
+	}
 }
